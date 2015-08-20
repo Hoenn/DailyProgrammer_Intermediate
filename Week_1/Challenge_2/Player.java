d7fc902f58c797fb29417099cd54db4c68710cc1
Hoenn <skatevan1313@gmail.com>
2015-08-20 10:07:16 -0400

    Completed Challenge 2

---
+++ Week_1/Challenge_2/Player.java
@@ -0,0 +1,35 @@
+package Challenge_2;
+
+import java.util.ArrayList;
+
+public class Player
+{
+	private String name;
+	private ArrayList<String> inventory;
+	public Player()
+	{
+		name="";
+		inventory = new ArrayList<String>();
+	}
+	public void setName(String s)
+	{
+		name= s;
+	}
+	public String getName()
+	{
+		return name;
+	}
+	public ArrayList<String> getInventory()
+	{
+		return inventory;
+	}
+	public void giveItem(String s)
+	{
+		if(!inventory.contains(s))
+			inventory.add(s);
+	}
+	public boolean hasItem(String s)
+	{
+		return inventory.contains(s);
+	}
+}
