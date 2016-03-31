778dff119aca5cfa93e1f042c08de5d4cafd55b1
Hoenn <skatevan1313@gmail.com>
2016-03-31 09:36:29 -0400

    Added a few comments

--- src/Challenge_39/Challenge_39.java
+++ src/Challenge_39/Challenge_39.java
@@ -9,14 +9,16 @@ import java.util.Scanner;
 
 public class Challenge_39 {
 	public static void main(String args[]) {
-		System.out.println("Enter number to check: ");
+		System.out.println("Enter a number to check: ");
 		Scanner input = new Scanner(System.in);
 		
 		int num = input.nextInt();
 		int sqr = (int)(Math.pow(num, 2));
 		
 		String sqrString = String.valueOf(sqr);
+		//left half
 		int left = Integer.valueOf(sqrString.substring(0, sqrString.length()/2));
+		//right half
 		int right = Integer.valueOf(sqrString.substring(sqrString.length()/2));
 
 		
@@ -25,7 +27,7 @@ public class Challenge_39 {
 			System.out.println(num+"^2 = "+sqr+" then "+left+"+"+right+"= "+num);
 		}
 		else {
-			System.out.println("No, "+num+" is not a Kaprekar number. '9' is an example of a Kaprekar number");
+			System.out.println("No, "+num+" is not a Kaprekar number. '9' is a good example of a Kaprekar number");
 		}
 		
 		input.close();
